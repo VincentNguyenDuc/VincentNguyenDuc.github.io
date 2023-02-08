374f35d62b928b2056e5b5c28240ef7ad22afe38
Vincent <thanhvinhnguyenduc@gmail.com>
2023-02-08 15:47:06 +0700

    add light and dark theme

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,7 @@ window.addEventListener("scroll", () => {
 
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
+let toggle = document.getElementById("toggle_light_dark");
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
@@ -14,4 +15,13 @@ menu.onclick = () => {
 window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('open');
+}
+
+toggle.onclick = () => {
+    document.body.classList.toggle("light_theme")
+    if (document.body.classList.contains("light_theme")) {
+        toggle.className = "fa-solid fa-sun";
+    } else {
+        toggle.classList = "fa-solid fa-moon";
+    }
 }
